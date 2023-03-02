71f1ea46f23625866bf008c62138adf290c97607
Kimhyeyun <cqqwer@gmail.com>
2023-03-02 20:17:18 +0900

    #30 - 데이터베이스에서 약국을 dto로 변환해 가져오는 클래스 생성
    redis 추후 추가 예정

---
+++ src/main/java/dev/be/pharmacyrecommendation/pharmacy/service/PharmacySearchService.java
@@ -0,0 +1,38 @@
+package dev.be.pharmacyrecommendation.pharmacy.service;
+
+import dev.be.pharmacyrecommendation.pharmacy.dto.PharmacyDto;
+import dev.be.pharmacyrecommendation.pharmacy.entity.Pharmacy;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PharmacySearchService {
+
+    private final PharmacyRepositoryService pharmacyRepositoryService;
+
+    public List<PharmacyDto> searchPharmacyDtoList() {
+        // redis
+
+        // db
+        return pharmacyRepositoryService.findAll()
+                .stream()
+                .map(this::convertToPharmacyDto)
+                .collect(Collectors.toList());
+    }
+
+    private PharmacyDto convertToPharmacyDto(Pharmacy pharmacy) {
+        return PharmacyDto.builder()
+                .id(pharmacy.getId())
+                .pharmacyName(pharmacy.getPharmacyName())
+                .pharmacyAddress(pharmacy.getPharmacyAddress())
+                .latitude(pharmacy.getLatitude())
+                .longitude(pharmacy.getLongitude())
+                .build();
+    }
+}
