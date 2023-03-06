255955700c648d4694d42b5ff5b39223fd1999a8
Kimhyeyun <cqqwer@gmail.com>
2023-03-06 23:11:46 +0900

    #41 - 입력 주소와 결과로 보여줄 약국 정보에 대한 DTO 생성

---
+++ src/main/java/dev/be/pharmacyrecommendation/direction/dto/InputDto.java
@@ -0,0 +1,11 @@
+package dev.be.pharmacyrecommendation.direction.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class InputDto {
+
+    private String address;
+}
