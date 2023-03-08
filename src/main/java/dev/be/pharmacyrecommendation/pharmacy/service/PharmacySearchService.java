46f8cc9920069785d1e876d87bf7c0e406bd2de7
Kimhyeyun <cqqwer@gmail.com>
2023-03-08 14:13:39 +0900

    #58 - 기존에 약국 리스트를 db에서 가져오는 코드 전에 성능 개선을 위해 redis를 먼저 살펴보는 코드 추가

--- src/main/java/dev/be/pharmacyrecommendation/pharmacy/service/PharmacySearchService.java
+++ src/main/java/dev/be/pharmacyrecommendation/pharmacy/service/PharmacySearchService.java
@@ -1,9 +1,11 @@
 package dev.be.pharmacyrecommendation.pharmacy.service;
 
+import dev.be.pharmacyrecommendation.pharmacy.cache.PharmacyRedisTemplateService;
 import dev.be.pharmacyrecommendation.pharmacy.dto.PharmacyDto;
 import dev.be.pharmacyrecommendation.pharmacy.entity.Pharmacy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,9 +17,12 @@ import java.util.stream.Collectors;
 public class PharmacySearchService {
 
     private final PharmacyRepositoryService pharmacyRepositoryService;
+    private final PharmacyRedisTemplateService pharmacyRedisTemplateService;
 
     public List<PharmacyDto> searchPharmacyDtoList() {
         // redis
+        List<PharmacyDto> pharmacyDtoList = pharmacyRedisTemplateService.findAll();
+        if(CollectionUtils.isNotEmpty(pharmacyDtoList)) return pharmacyDtoList;
 
         // db
         return pharmacyRepositoryService.findAll()
